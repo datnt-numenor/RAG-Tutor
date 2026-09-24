db48334e4d7abb6dc04f0f678d7ba7511e9b6095
datnt-numenor <datksnb2005@gmail.com>
2026-09-24 16:45:24 +0700

    chore: remove unordered migration 20260924_question_embedding_and_dedup.sql

--- backend/migrations/20260924_question_embedding_and_dedup.sql
+++
@@ -1,9 +0,0 @@
--- Semantic deduplication support for generated questions.
-set search_path = public, extensions;
-
-alter table public.questions
-  add column if not exists question_embedding vector(384);
-
-create index if not exists questions_embedding_hnsw_idx
-  on public.questions using hnsw (question_embedding vector_cosine_ops)
-  where question_embedding is not null;
