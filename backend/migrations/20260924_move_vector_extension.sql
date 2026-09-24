c2df4573a962cf3bfb1dc5b7ce6a1a1aeacb6d0e
datnt-numenor <datksnb2005@gmail.com>
2026-09-24 16:46:56 +0700

    chore: remove unordered pgvector migration

--- backend/migrations/20260924_move_vector_extension.sql
+++
@@ -1,6 +0,0 @@
--- Move pgvector out of public while keeping match_chunks operator resolution intact.
-
-alter function public.match_chunks(uuid, vector, integer, double precision)
-  set search_path = public, extensions;
-
-alter extension vector set schema extensions;
